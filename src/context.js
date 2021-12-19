c95baa5b15d8956bb1e3f3d4772028c14e435239
unknown <aflaharif2005@gmail.com>
2021-12-19 16:17:43 +0500

    Hotel APP

---
+++ src/context.js
@@ -0,0 +1,217 @@
+import React, { Component, useState,useEffect,useCallback} from 'react';
+import items from './data';
+
+const RoomContext = React.createContext();
+
+// export const AuthContext = React.createContext({
+//   token: '',
+//   isLoggedIn: false,
+//   login: (token) => {},
+//   logout: () => {},
+// });
+
+// const calculateRemainingTime = (expirationTime) => {
+//   const currentTime = new Date().getTime();
+//   const adjExpirationTime = new Date(expirationTime).getTime();
+
+//   const remainingDuration = adjExpirationTime - currentTime;
+
+//   return remainingDuration;
+// };
+
+// const retrieveStoredToken = () => {
+//   const storedToken = localStorage.getItem('token');
+//   const storedExpirationDate = localStorage.getItem('expirationTime');
+
+//   const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+//   if (remainingTime <= 3600) {
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('expirationTime');
+//     return null;
+//   }
+
+//   return {
+//     token: storedToken,
+//     duration: remainingTime,
+//   };
+// };
+
+// export const AuthContextProvider = (props) => {
+//   const tokenData = retrieveStoredToken();
+  
+//   let initialToken;
+//   if (tokenData) {
+//     initialToken = tokenData.token;
+//   }
+
+//   const [token, setToken] = useState(initialToken);
+
+//   const userIsLoggedIn = !!token;
+
+//   const logoutHandler = useCallback(() => {
+//     setToken(null);
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('expirationTime');
+
+//     if (logoutTimer) {
+//       clearTimeout(logoutTimer);
+//     }
+//   }, []);
+
+//   const loginHandler = (token, expirationTime) => {
+//     setToken(token);
+//     localStorage.setItem('token', token);
+//     localStorage.setItem('expirationTime', expirationTime);
+
+//     const remainingTime = calculateRemainingTime(expirationTime);
+
+//     logoutTimer = setTimeout(logoutHandler, remainingTime);
+//   };
+
+//   useEffect(() => {
+//     if (tokenData) {
+//       console.log(tokenData.duration);
+//       logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+//     }
+//   }, [tokenData, logoutHandler]);
+
+//   const contextValue = {
+//     token: token,
+//     isLoggedIn: userIsLoggedIn,
+//     login: loginHandler,
+//     logout: logoutHandler,
+//   };
+
+//   return (
+//     <AuthContext.Provider value={contextValue}>
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+export default class RoomProvider extends Component {
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+    //
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
+  };
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+      //
+      price: maxPrice,
+      maxPrice,
+      maxSize,
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    console.log(name, value);
+
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
+  };
+  filterRooms = () => {
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state;
+
+    let tempRooms = [...rooms];
+
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    });
+  };
+  render() {
+    return (
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {(value) => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
